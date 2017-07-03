972edac22f763d918dbe62b2b777e0dc23fa545f
AlbertoSig <alberto.signori1@gmail.com>
2017-07-03 22:09:20 +0200

    File for data extraction, to copy in desert_samples directory

---
+++ dataExtraction.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+rm my_log.txt x.txt y.txt z.txt data.txt
+
+ns $1 >> my_log.txt
+
+grep -Po 'x = ([-0-9.\n]*[,]*)' $2 | grep -o '[-0-9.]*' >> x.txt
+grep -Po 'y = ([-0-9.\n]*[,]*)' $2 | grep -o '[-0-9.]*' >> y.txt
+grep -Po 'z = ([-0-9.\n]*[,]*)' $2 | grep -o '[-0-9.]*' >> z.txt
+
+
+grep -io 'applicationROV Throughput[ ]* : [0-9.]*' my_log.txt | grep -io [0-9.]* >> data.txt
+grep -io 'applicationROV PER[ ]* : [-0-9.]*' my_log.txt | grep -io [0-9.]*  >> data.txt
+grep -io 'applicationCTR Throughput[ ]* : [0-9.]*' my_log.txt | grep -io [0-9.]* >> data.txt
+grep -io 'applicationCTR PER[ ]* : [-0-9.]*' my_log.txt | grep -io [0-9.]*  >> data.txt
+grep -io 'Sent Packets[ ]*: [0-9]*' my_log.txt | grep -io [0-9]* >> data.txt
+grep -io 'Received[ ]*: [0-9]*' my_log.txt | grep -io [0-9]* >> data.txt
+grep -io 'ROV period[]*: [0-9]*' my_log.txt | grep -io [0-9]* >> data.txt
+grep -i 'adaptive' my_log.txt | grep -io [0-9] >> data.txt
+grep -i 'constant' my_log.txt | grep -io [0-9] >> data.txt
+
+cp ./x.txt  /media/sf_VirtualMachine/Project_Simulation/matlab
+cp ./y.txt  /media/sf_VirtualMachine/Project_Simulation/matlab
+cp ./z.txt  /media/sf_VirtualMachine/Project_Simulation/matlab
+cp ./data.txt  /media/sf_VirtualMachine/Project_Simulation/matlab
