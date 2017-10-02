cca16dfe690ccf26b6030e9778126fc534719c4e
AlbertoSig <alberto.signori1@gmail.com>
2017-10-02 22:18:29 +0200

    02/10/17 22.18

---
+++ matlab/bestSlotTDMA_bruteForce.m
@@ -0,0 +1,168 @@
+clear;
+
+N_nodes = 6;
+N_slots = 7;
+queue = zeros(N_nodes,10); %riga i = coda nodo i
+index = ones(N_nodes,1); %riga i = indice coda i;
+
+i = 0;
+slot = 0;
+rx_ROV = zeros(1,10); %pacchetti del ROV ricevuti dal CTR
+rx_ROV_index = 1;
+tx_ROV = zeros(1,10); %pacchetti trasmessi dal ROV
+tx_ROV_index = 1;
+
+rx_CTR = zeros(1,10); %pacchetti del CTR ricevuti dal ROV
+rx_CTR_index = 1;
+tx_CTR = zeros(1,10); %pacchetti trasmessi dal CTR
+tx_CTR_index = 1;
+
+
+v1 = [1;0;0;1;0;0];
+v2 = [0;0;0;1;0;0];
+v3 = [0;1;0;0;1;0];
+v4 = [0;1;0;0;1;0];
+v5 = [0;0;1;0;0;0];
+v6 = [0;0;1;0;0;0];
+v7 = [0;0;0;0;0;1];
+
+m = [v1,v2,v3,v4,v5,v6,v7];
+
+MAX_packet_sent = 100;
+packet_sent = 0;
+
+N_iteration = 10*7*6*5*4*3*2;
+
+[row,column] = size(m);
+rand_matrix = zeros(row,column,N_iteration);
+average_ROV_delay = zeros(1,N_iteration);
+average_CTR_delay = zeros(1,N_iteration);
+
+for j=1:N_iteration
+    %estrazione matrice
+    %y = datasample(m,N_slots,2,'Replace',false);
+    rand_index = randsample(N_slots,N_slots);
+    y = m(:,rand_index);
+    queue = zeros(N_nodes,10); %riga i = coda nodo i
+    index = ones(N_nodes,1); %riga i = indice coda i;
+    
+    packet_sent = 0;
+    i = 0;
+    slot = 0;
+    rx_ROV = zeros(1,10); %pacchetti del ROV ricevuti dal CTR
+    rx_ROV_index = 1;
+    tx_ROV = zeros(1,10); %pacchetti trasmessi dal ROV
+    tx_ROV_index = 1;
+
+    rx_CTR = zeros(1,10); %pacchetti del CTR ricevuti dal ROV
+    rx_CTR_index = 1;
+    tx_CTR = zeros(1,10); %pacchetti trasmessi dal CTR
+    tx_CTR_index = 1;
+    
+    %y = [v1,v3,v5,v6,v2,v4,v7];
+    while packet_sent < MAX_packet_sent
+        for k=1:N_slots
+            slot = slot + 1;
+            index_matrix = find(y(:,k) == 1);
+            for n=1:length(index_matrix)
+                
+                node = index_matrix(n);
+                
+                if node == 1 %trasmette il CTR
+                    queue(node,1) = 1;
+                    index(node) = index(node) + 1;
+                    
+                    if queue(node,1) == 1
+                        queue(node,1:end-1) = queue(node,2:end);
+                        index(node) = index(node) - 1;
+                        queue(node+1, index(node+1)) = 1;
+                        index(node+1) = index(node +1) + 1;
+                        tx_CTR(tx_CTR_index) = slot;
+                        tx_CTR_index = tx_CTR_index + 1;
+                    end
+                else
+                    if node == N_nodes %trasmette il ROV
+                        queue(node,1) = 2;
+                        index(node) = index(node) + 1;
+                        
+                        if queue(node,1) == 2
+                            queue(node,1:end-1) = queue(node,2:end);
+                            index(node) = index(node) - 1;
+                            queue(node-1, index(node-1)) = 2;
+                            index(node-1) = index(node -1) + 1;
+                            tx_ROV(tx_ROV_index) = slot;
+                            tx_ROV_index = tx_ROV_index + 1;
+                        end
+                    else %trasmette un nodo intermedio
+                        %%devo diversificare se sono i due relay estremi oppure
+                        %%no
+                        if node == 2 %relay 1
+                            if queue(node,1) == 1 %trasmetto verso R2
+                                queue(node,1:end-1) = queue(node,2:end);
+                                index(node) = index(node) -1;
+                                queue(node+1,index(node+1)) = 1;
+                                index(node + 1) = index(node +1) + 1;
+                            else
+                                if queue(node,1) == 2 %pacchetto trasmesso verso CTR
+                                    queue(node,1:end-1) = queue(node,2:end);
+                                    index(node) = index(node) -1;
+                                    rx_ROV(rx_ROV_index) = slot;
+                                    rx_ROV_index = rx_ROV_index + 1;
+                                end
+                            end
+                            
+                        else
+                            if node == N_nodes -1 %ultimo relay
+                                if queue(node,1) == 1 %trasmetto verso ROV
+                                    queue(node,1:end-1) = queue(node,2:end);
+                                    index(node) = index(node) -1;
+                                    rx_CTR(rx_CTR_index) = slot;
+                                    rx_CTR_index = rx_CTR_index + 1;
+                                else
+                                    if queue(node,1) == 2 %pacchetto trasmesso verso CTR
+                                        queue(node,1:end-1) = queue(node,2:end);
+                                        index(node) = index(node) -1;
+                                        queue(node-1,index(node-1)) = 2;
+                                        index(node - 1) = index(node -1) + 1;
+                                    end
+                                end
+                            else %relay qualsiasi
+                                if queue(node,1) == 1 %trasmetto verso ROV
+                                    queue(node,1:end-1) = queue(node,2:end);
+                                    index(node) = index(node) -1;
+                                    queue(node+1,index(node+1)) = 1;
+                                    index(node + 1) = index(node +1) + 1;
+                                else
+                                    if queue(node,1) == 2 %pacchetto trasmesso verso CTR
+                                        queue(node,1:end-1) = queue(node,2:end);
+                                        index(node) = index(node) -1;
+                                        queue(node-1,index(node-1)) = 2;
+                                        index(node - 1) = index(node -1) + 1;
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end %fine FOR su index
+        end
+        packet_sent = packet_sent + 1;
+    end %end while
+    average_ROV_delay(j) = sum(rx_ROV(1:rx_ROV_index-1) - tx_ROV(1:rx_ROV_index-1)+1)/(rx_ROV_index-1);
+    average_CTR_delay(j) = sum(rx_CTR(1:rx_CTR_index-1) - tx_CTR(1:rx_CTR_index-1)+1)/(rx_CTR_index-1);
+    rand_matrix(:,:,j) = y;
+
+end
+
+[min_CTR,min_CTR_index] = min(average_CTR_delay);
+min_CTR_matrix = rand_matrix(:,:,min_CTR_index);
+all_min_CTR_index = find( average_CTR_delay == min_CTR); 
+[min_ROV,min_ROV_index] = min(average_ROV_delay);
+min_ROV_matrix = rand_matrix(:,:,min_ROV_index);
+all_min_ROV_index = find( average_ROV_delay == min_ROV); 
+[min_sum,min_sum_index] = min(average_ROV_delay+average_CTR_delay);
+average_sum_delay = average_ROV_delay+average_CTR_delay;
+min_sum_matrix = rand_matrix(:,:,min_sum_index);
+all_min_sum_index = find( average_sum_delay == min_sum); 
+
+
---
+++ matlab/posBasedRT_matrixComparison.m
@@ -0,0 +1,152 @@
+clear;
+range = 1:1:60;
+save_flag = 1;
+dir = 'classicMatrix_sinPath_twp50s_ROVqueue2';
+load(['PosBasedRouting/',dir,'/TDMApipeline/figure/simPosBasedRt_NewPath.mat']);
+%max_ROV_period_theo = slot_duration*3;
+%rmse
+figure(1);
+semilogy(range, adap_rmse,'-r*','MarkerSize',5);
+grid on;
+hold on;
+%semilogy(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'--k');
+axis([1 60 10^-3 10^4]);
+title('RMSE');
+xlabel('ROV period [s]');
+ylabel('RMSE');
+%ROV packet delivery ratio
+figure(2)
+plot(range, adap_ROV_pdr,'-r*','MarkerSize',5);
+grid on;
+hold on;
+%plot(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'--k');
+axis([1 60 0 1]);
+title('ROV packet delivery ratio');
+xlabel('ROV period [s]');
+ylabel('ROV packet delivery ratio');
+%CTR packet delivery ratio
+figure(3)
+plot(range, adap_CTR_pdr,'-r*','MarkerSize',5);
+grid on;
+hold on;
+%plot(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'k--');
+axis([1 60 0 1]);
+title('CTR packet delivery ratio');
+xlabel('ROV period [s]');
+ylabel('CTR packet delivery ratio');
+%ROV packet delivry delay
+figure(4);
+plot(range, adap_ROV_pdd,'-r*','MarkerSize',5);
+axis([1 60 0 2500]);
+grid on;
+hold on;
+%plot(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'k--');
+title('ROV packet delivery delay');
+xlabel('ROV period [s]');
+ylabel('ROV Packet delivery delay [s]');
+%CTR packet delivery delay
+figure(5)
+plot(range, adap_CTR_pdd,'-r*','MarkerSize',5);
+grid on;
+hold on;
+%plot(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'k--');
+title('CTR packet delivery delay');
+xlabel('ROV period [s]');
+ylabel('CTR packet delivery delay [s]');
+axis([1 60 0 800]);
+%CTR throughput
+figure(6)
+plot(range, adap_CTR_th,'-r*','MarkerSize',5);
+grid on;
+hold on;
+%plot(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'k--');
+title('CTR throughput');
+xlabel('ROV period [s]');
+ylabel('CTR throughput [bit/s]');
+
+axis([1 60 0 750]);
+%ROV throughput
+figure(7)
+plot(range, adap_ROV_th,'-r*','MarkerSize',5);
+grid on;
+hold on;
+%plot(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'k--');
+title('ROV throughput');
+xlabel('ROV period [s]');
+ylabel('ROV throughput [bit/s]');
+axis([1 60 0 450]);
+
+
+dir = 'newMatrix3_sinPath_twp50s_ROVqueue2';
+load(['PosBasedRouting/',dir,'/TDMApipeline/figure/simPosBasedRt_NewPath.mat']);
+%max_ROV_period_theo = slot_duration*3;
+%rmse
+figure(1);
+semilogy(range, adap_rmse,'-b.','MarkerSize',12);
+grid on;
+hold on;
+%semilogy(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'--k');
+title('RMSE');
+xlabel('ROV period [s]');
+ylabel('RMSE');
+%ROV packet delivery ratio
+figure(2)
+plot(range, adap_ROV_pdr,'-b.','MarkerSize',12);
+grid on;
+hold on;
+%plot(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'--k');
+axis([1 60 0 1]);
+title('ROV packet delivery ratio');
+xlabel('ROV period [s]');
+ylabel('ROV packet delivery ratio');
+%CTR packet delivery ratio
+figure(3)
+plot(range, adap_CTR_pdr,'-b.','MarkerSize',12);
+grid on;
+hold on;
+%plot(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'k--');
+axis([1 60 0 1]);
+title('CTR packet delivery ratio');
+xlabel('ROV period [s]');
+ylabel('CTR packet delivery ratio');
+%ROV packet delivry delay
+figure(4);
+plot(range, adap_ROV_pdd,'-b.','MarkerSize',12);
+axis([1 60 0 500]);
+grid on;
+hold on;
+%plot(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'k--');
+title('ROV packet delivery delay');
+xlabel('ROV period [s]');
+ylabel('ROV Packet delivery delay [s]');
+%CTR packet delivery delay
+figure(5)
+plot(range, adap_CTR_pdd,'-b.','MarkerSize',12);
+grid on;
+hold on;
+%plot(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'k--');
+title('CTR packet delivery delay');
+xlabel('ROV period [s]');
+ylabel('CTR packet delivery delay [s]');
+axis([1 60 0 100]);
+%CTR throughput
+figure(6)
+plot(range, adap_CTR_th,'-b.','MarkerSize',12);
+grid on;
+hold on;
+%plot(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'k--');
+title('CTR throughput');
+xlabel('ROV period [s]');
+ylabel('CTR throughput [bit/s]');
+
+axis([1 60 0 750]);
+%ROV throughput
+figure(7)
+plot(range, adap_ROV_th,'-b.','MarkerSize',12);
+grid on;
+hold on;
+%plot(ones(1,2)*max_ROV_period_theo,[10^-5 , 10^5],'k--');
+title('ROV throughput');
+xlabel('ROV period [s]');
+ylabel('ROV throughput [bit/s]');
+axis([1 60 0 450]);
