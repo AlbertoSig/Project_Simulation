0679c1582d4856480d732bc1d75b7bb00f5ee708
alberto_signori <alberto.signori@studenti.unipd.it>
2017-11-23 15:33:10 +0100

    23/11/17 15.33

--- matlab/BW_3dB.m
+++ matlab/BW_3dB.m
@@ -1,29 +1,18 @@
-clear;
-f = 25;
-B = 5;
+function [f_min,f_max,f_0] = BW_3dB(d)
+
 k = 1.5;
-d = 3;
 s = 0.5;
 w= 0;
-    P_dB = 181;
-    P = 10^(P_dB/10);
-
-delta = 0.01;
-freq = 22.5:delta:27.5;
+delta = 0.1;
+freq = 0.1:delta:100; %KHz
+snr= zeros(1,length(freq));
 i = 0;
-dist = 0.1:0.1:100;
-f_0 = zeros(1,length(dist));
-for d=1:0.1:100
-    i = i+1;
-    [f_min,f_max,f_0(i)] = BW_3dB(10);
-end
-plot(dist,f_0);
 
 for f = freq
     i = i+1;
     a_dB = 0.11*f^2/(1+f^2) + 44*f^2/(4100+f)+f^2*2.75*10^(-4)+0.003;
     a = 10^(a_dB/10);
-
+    
     
     N_t_dB= 17 - 30*log10(f);
     N_t = 10^(N_t_dB/10);
@@ -35,18 +24,17 @@ for f = freq
     N_th = 10^(N_th_dB/10);
     N = N_t+N_th+N_s+N_w;
     %N = 10^(N_dB/10);
-%     
-%     A_dB = k*log10(d*100) + d*log10(a_dB);
-%     A2 = 10^(A_dB/10);
+    %
+    %     A_dB = k*log10(d*100) + d*log10(a_dB);
+    %     A2 = 10^(A_dB/10);
     A = (d*1000)^k * a^(d);
-
     
-    snr_dB(i) = 10*log10(P/(A*N*length(freq)));
-    snr(i) = P/(A*N*B);
+    snr(i) = 1/(A*N);
+    
 end
-figure(1);
-plot(freq,snr_dB)
-hold on
-axis([22.5 27.5 -170 -70]);
-B = 5000;
-c = delta*sum(log2(1+snr));
+plot(freq,10*log10(snr));
+[snr_0,index] = max(snr);
+f_0 = freq(index);
+index_Bw = find(snr > snr_0/2);
+f_min = freq(index_Bw(1));
+f_max = freq(index_Bw(end));
